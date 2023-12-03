93c01ad6347761a30e039bf3c135ed458804883f
deasun6 <dsunjic@pmfst.hr>
2023-12-03 21:34:38 +0100

    Vjezbe4

--- Vjezbe4/analyze.cpp
+++ Vjezbe4/analyze.cpp
@@ -7,8 +7,8 @@ int main()
 
   analyzer = new Analyzer();
 
-  analyzer->ReadTxtFile("../Vjezbe3/Zad_3/Analysis.txt");
-  analyzer->ConvertTxtToRootFile("../Vjezbe3/Zad_3/Analysis.txt","Analysis.root");
+  analyzer->ReadTxtFile("../Vjezbe3/Zadatak3/Analysis.txt");
+  analyzer->ConvertTxtToRootFile("../Vjezbe3/Zadatak3/Analysis.txt","Analysis.root");
 
   analyzer->~Analyzer();
 
